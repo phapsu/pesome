f1b0e4461804777137e6f7856b51f22f2a3f685c
Vu Khanh Truong <vukhanhtruong@gmail.com>
2012-12-11 08:56:26 +0700

    add controller.js

---
+++ controllers.js
@@ -0,0 +1,14 @@
+// steal model files
+steal(
+    'pesome/petopic/create',
+    'pesome/petopic/list',
+    'pesome/petopic_detail/peticks_list',
+    'pesome/petopic_detail/list',
+    'pesome/petick/link',
+    'pesome/petick/video',
+    'pesome/petick/audio',
+    'pesome/petick/share_file',
+    'pesome/petick/photo_album',
+    'pesome/petick/text',        
+    'pesome/login/create'
+)
--- pesome.js
+++ pesome.js
@@ -10,32 +10,22 @@ steal(
 ).then(
     './js/config.js',		// config app
     './models/models.js',		// steals all your models
-    //'./fixtures/fixtures.js',	// sets up fixtures for your models    
+    './controllers.js',		// steals all your controllers
+    //'./fixtures/fixtures.js',	// sets up fixtures for your models
     './js/video.js',
-    'pesome/petopic/create',
-    'pesome/petopic/list',
-    'pesome/petopic_detail/peticks_list',
-    'pesome/petopic_detail/list',
-    'pesome/petick/link',
-    'pesome/petick/video',
-    'pesome/petick/audio',
-    'pesome/petick/share_file',
-    'pesome/petick/photo_album',
-    'pesome/petick/text',        
-	'pesome/login/create',
     function(){
         // configure your application
         $('#petopics').pesome_petopic_list();
         //$('#petopics_create').pesome_petopic_create();
         $('#petopic_details').pesome_petopic_detail_list();
         $('#petopic_peticks_list').pesome_petopic_detail_peticks_list();
-    
+
     	$('#petick_link').pesome_petick_link();
     	$('#petick_video').pesome_petick_video();
     	$('#petick_photo_gallery').pesome_petick_photo_album();
     	$('#petick_audio').pesome_petick_audio();
-		$('#login').pesome_login_create();
+        $('#login').pesome_login_create();
 }).then(
     {src : './js/jquery.mobile-1.2.0.min.js', compress : false},
-    {src : './js/photoswipe.min.js', compress : false}    
+    {src : './js/photoswipe.min.js', compress : false}
 )
